ba99964aff65f649e22591d012d6b2944ab738d8
alfatauri10 <alfandari.jacopo.stud@itisgalileiroma.it>
2025-04-13 13:46:56 +0200

    modifica netlify.toml

--- src/js/language-switcher.js
+++ src/js/language-switcher.js
@@ -23,7 +23,7 @@ window.LanguageSwitcher = {
                 window.location.href = window.location.pathname.replace('src/pages/menu/it/contatti.html', 'src/pages/menu/en/contattiEN.html');
             }else if(window.location.pathname.includes('index.html')) {
                 window.location.href = window.location.pathname.replace('lostInRome/index.html', 'lostInRome/indexEN.html');
-            }else if(window.location.pathname.includes('/index.html')) {
+            }else if(window.location.pathname.includes('')) {
                 window.location.href = window.location.pathname.replace('lostInRome/index.html', 'lostInRome/indexEN.html');
            }
         } else {
@@ -39,7 +39,7 @@ window.LanguageSwitcher = {
                 window.location.href = window.location.pathname.replace('src/pages/menu/en/contattiEN.html', 'src/pages/menu/it/contatti.html');
             }else if(window.location.pathname.includes('indexEN.html')) {
                 window.location.href = window.location.pathname.replace('lostInRome/indexEN.html', 'lostInRome/index.html');
-            }else if(window.location.pathname.includes('/indexEN.html')) {
+            }else if(window.location.pathname.includes('')) {
                 window.location.href = window.location.pathname.replace('lostInRome/indexEN.html', 'lostInRome/index.html');
             }
 
