33ede4a4842a1ce0ae6dd9bfcee8bafc4c264a0c
alfatauri10 <alfandari.jacopo.stud@itisgalileiroma.it>
2025-05-11 09:25:53 +0200

    change -> with > and <- with <. Da validare.

--- src/js/gallery-monuments-scroll.js
+++ src/js/gallery-monuments-scroll.js
@@ -10,8 +10,10 @@ function initGallery() {
         cssEase: 'ease-in-out',
         autoplay: true,
         autoplaySpeed: 5000,
-        prevArrow: '<button type="button" class="slick-prev">←</button>',
-        nextArrow: '<button type="button" class="slick-next">→</button>'
+        /*prevArrow: '<button type="button" class="slick-prev">←</button>',*/
+        /*nextArrow: '<button type="button" class="slick-next">→</button>'*/
+        prevArrow: '<button type="button" class="slick-prev"><</button>',
+        nextArrow: '<button type="button" class="slick-next">></button>'
     });
 }
 
